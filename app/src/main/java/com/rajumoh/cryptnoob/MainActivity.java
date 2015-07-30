8851d1cf6fae97dcf8bcc0864fa40b6033394133
flyingarg <mohit.raju.m@gmail.com>
2015-07-30 17:51:39 +0300

    user scripts

--- app/src/main/java/com/rajumoh/cryptnoob/MainActivity.java
+++ app/src/main/java/com/rajumoh/cryptnoob/MainActivity.java
@@ -1,21 +1,22 @@
 package com.rajumoh.cryptnoob;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-
-import android.os.Bundle;
-
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
@@ -95,11 +96,49 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             int section = this.getArguments().getInt(ARG_SECTION_NUMBER);
             if ( section == 1){
-                View rootView = inflater.inflate(R.layout.fragment_1, container, false);
+                final View rootView = inflater.inflate(R.layout.fragment_1, container, false);
+                EditText algoSave = (EditText)rootView.findViewById(R.id.algo_save);
+                EditText testTextSave = (EditText)rootView.findViewById(R.id.test_text_save);
+                algoSave.setText("" +
+                        "public encryptTest(plainText){\n" +
+                        " String response = \"\";\n" +
+                        " for(int i=0; i<plainText.length(); i++){\n" +
+                        "  response += (char)((((int)plainText.charAt(i))+21)%254);\n" +
+                        " }\n" +
+                        " return response;\n" +
+                        "}\n" +
+                        "\n" +
+                        "public decryptTest(encString){\n" +
+                        " String response = \"\";\n" +
+                        " for(int i=0; i<encString.length(); i++){\n" +
+                        "  response += (char)((((int)encString.charAt(i))+233)%254);\n" +
+                        " }\n" +
+                        " return response;\n" +
+                        "}\n");
+                testTextSave.setText("Test plain text");
+                Button runTest = (Button)rootView.findViewById(R.id.run_test);
+                runTest.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText algoSave = (EditText)rootView.findViewById(R.id.algo_save);
+                        EditText testTextSave = (EditText)rootView.findViewById(R.id.test_text_save);
+                        String encryptTest = "encryptTest(testString);\n";
+                        String encDecTest = "decryptTest(encryptTest(testString));\n";
+
+                        GrooidShell shell = new GrooidShell(getActivity().getApplicationContext().getDir("dynclasses", 0), this.getClass().getClassLoader());
+                        Log.i("rajumoh", "Evaluating EncryptionTest : " + shell.evaluate(algoSave.getText()+"\ntestString = \""+testTextSave.getText()+"\"\n"+encryptTest).getResult());
+                        Log.i("rajumoh", "Evaluating EncDecTest : " + shell.evaluate(algoSave.getText()+"\ntestString = \""+testTextSave.getText()+"\"\n"+encDecTest).getResult());
+                        String response = shell.evaluate(algoSave.getText()+"\ntestString = \""+testTextSave.getText()+"\"\n"+encDecTest).getResult();
+                        if(response.equals(testTextSave.getText().toString())) {
+                            Toast.makeText(getActivity().getApplicationContext(), "Test Success.", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(getActivity().getApplicationContext(), "Test Failed. Your Crypt does not seem to be working", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
                 return rootView;
             }else if( section == 2){
                 View rootView = inflater.inflate(R.layout.fragment_2, container, false);
@@ -117,8 +156,7 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         @Override
         public void onAttach(Activity activity) {
             super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
+            ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
 }
